a1146a6297274a5e96a11cf1117c34cf23795154
eduoop <85969484+eduoop@users.noreply.github.com>
2022-05-14 00:25:08 -0300

    recentSearch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useState } from 'react';
 import styles from './Home.module.css'
 import Loader from "../layouts/Loader"
+
+import { useState } from 'react';
+
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,12 +14,9 @@ export default function Home() {
 
     const [username, setUserName] = useState('')
     const [user, setUser] = useState({})
-    const [recentUsers, setRecentUsers] = ([])
+    const [recentUsers, setRecentUsers] = useState([])
     const [removeLoading, setRemoveLoading] = useState(true)
     const navigate = useNavigate()
-
-    const usersR = []
-
 
     const submit = (e) => {
         e.preventDefault();
@@ -26,11 +25,10 @@ export default function Home() {
         axios.get(`https://api.github.com/users/${username}`)
         .then(async (res) => {
             await setUser(res.data)
-            usersR.push(res.data.login)
+            setRecentUsers(current => ([...current, res.data]))
             if(res.data.name) {
                 localStorage.setItem(`Usuario: ${res.data.name}`, res.data.name)
             }
-            console.log(usersR)
         })
         .catch((err) => {
             console.log(err)
@@ -45,29 +43,43 @@ export default function Home() {
 
 
     return (
-        <div className={styles.main}>
-            <div className={styles.app}>
-                <form>
-                    <input 
-                    type="text" 
-                    id='username'
-                    onChange={e => setUserName(e.target.value)} 
-                    placeholder="Username" 
-                    name="searchUser"/>
+        <div className={styles.general_container}>
+            <h1>HUBbusca</h1>
+            <div className={styles.main}>
+                <div className={styles.app}>
+                    <form>
+                        <input 
+                        type="text" 
+                        id='username'
+                        onChange={e => setUserName(e.target.value)} 
+                        placeholder="Username" 
+                        name="searchUser"/>
 
-                    <button onClick={submit}>Pesquisar usuário</button>
-                    {!removeLoading && <Loader/>}
-                </form>
+                        <button onClick={submit}>Pesquisar usuário</button>
+                        {!removeLoading && <Loader/>}
+                    </form>
 
-                {user && 
-                    <main className={styles.main_container}>
-                        <img src={user.avatar_url} onClick={toRepos}/>
-                        {user.name && <h1>{user.name}</h1>}
-                        {user.login && <h2> <MdOutlinePersonOutline/> {user.login}</h2>}
-                        {user.location && <h2> <FiMapPin/> {user.location}</h2>}
-                    </main>
-                }
+                    {user && 
+                        <main className={styles.main_container}>
+                            <img src={user.avatar_url} onClick={toRepos}/>
+                            {user.name && <h1>{user.name}</h1>}
+                            {user.login && <h2> <MdOutlinePersonOutline/> {user.login}</h2>}
+                            {user.location && <h2> <FiMapPin/> {user.location}</h2>}
+                        </main>
+                    }
+                </div>
             </div>
-       </div>
+                <div className={styles.recent_users}>
+                    <h1>Buscados recentemente:</h1>
+                            { recentUsers.map((user) => (
+                                <div className={styles.infos}>
+                                    <img onClick={toRepos} src={user.avatar_url} />
+                                    { user.name && <h1> {user.name}</h1> }
+                                    <h2> <MdOutlinePersonOutline/> {user.login}</h2>
+                                    <h2> <FiMapPin/> {user.location}</h2>
+                                </div>
+                            )) }
+                </div>
+        </div>
     )
 }
